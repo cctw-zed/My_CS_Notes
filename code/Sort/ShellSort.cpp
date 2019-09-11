6eb0550619c7f20fb21baebb025cd19a597f8c2c
Zed Zhao <u201413658@alumni.hust.edu.cn>
2019-09-11 17:03:20 +0800

    创建一个新文件夹

---
+++ code/Sort/ShellSort.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+void ShellSort(int nums[], int len)
+{
+  int h = 1;
+  while(h < len / 3)
+    h = 3 * h + 1;
+  while(h >= 1)
+  {
+    for(int i = h; )
+  }
+}
